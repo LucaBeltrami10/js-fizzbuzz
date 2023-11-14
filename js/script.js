7a3d8ee72725561c825db221cac4dce6f448f747
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-14 14:26:56 +0100

    inseriti commenti. esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 
-
+/* creo il ciclo per generare numeri da 1 a 100 */
 for (let i = 1; i <= 100; i++){
-    /* console.log(i) */
+    /*creo condizione che mi mostri quello che voglio in base al resto delle divisioni */
+    /* prima condizione: se il resto /3 e /5 è 0 -> fizzbuzz*/
+    /* seconda condizione: se il resto /3  è 0 -> fizz*/
+    /* terza condizione: se il resto  /5 è 0 -> buzz*/
+    /* altre condizioni: -> i*/
     if (i%3 == 0 && i%5 == 0){
         console.log('fizzBuzz')
     } else if(i%3 == 0){
